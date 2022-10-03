b443063e117ae53dab081e75b566a309921b4e14
mushruf12 <mushrufs@gmail.com>
2022-10-03 10:41:56 +0530

    commit

---
+++ oct-3/Question2/src/books/Books.java
@@ -0,0 +1,10 @@
+package books;
+
+public class Books {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ oct-3/Question2/src/papers/Papers.java
@@ -0,0 +1,10 @@
+package papers;
+
+public class Papers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
